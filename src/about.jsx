18c2fadcfc9c49296fb229b5a7e61bea27bed379
Greg Gamel <ggamel@gmail.com>
2018-01-07 23:08:19 -0600

    Fix YAML typo to fix dist build. 💪

--- src/about.jsx
+++ src/about.jsx
@@ -1,6 +1,7 @@
 ---
 title: About
 description: About Greg
+---
 <Box bg='tomato' color='white' p={4}>
   <h1>About Greg</h1>
   [@EyeGuide](https://eye.guide/)  —  Comprehensive Designer  —  Hardware & Software Products  —  UX & Usability Research  —  No-fuss  —  💚s green chile  —  !|!|
