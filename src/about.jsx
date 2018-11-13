d490d123c10e9adf6327891cfea17801e0e5ef2e
Greg Gamel <ggamel@gmail.com>
2018-11-12 23:17:46 -0600

    Out with the old

--- src/about.jsx
+++
@@ -1,8 +0,0 @@
----
-title: About
-description: About Greg
----
-<Box bg='tomato' color='white' p={4}>
-  <h1>About Greg</h1>
-  [@EyeGuide](https://eye.guide/)  —  Comprehensive Designer  —  Hardware & Software Products  —  UX & Usability Research  —  No-fuss  —  💚s green chile  —  !|!|
-</Box>
--- src/layout.jsx
+++
@@ -1,7 +0,0 @@
-<Box bg='black' p={4}>
-  <Box bg='black' py={4}>
-    <Box mx='auto' px={3} style={{ maxWidth: 768 }}>
-      {props.children}
-    </Box>
-  </Box>
-</Box>
