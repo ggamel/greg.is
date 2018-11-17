6d4468114fb670569b016a639fabb5f798bfc03b
Greg Gamel <ggamel@gmail.com>
2018-11-17 10:01:46 -0600

    Check deploy w/minor edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,7 @@ import React from "react";
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <h1>Hello Gatsby!</h1>
-    <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <h1>Greg Gamel</h1>
+    <p>Comprehensive Designer</p>
   </div>
 );
