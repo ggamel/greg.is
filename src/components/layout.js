0b91ae985ce3f601ca9f99fdc105da1fdc10b999
Greg Gamel <ggamel@gmail.com>
2019-04-18 23:41:23 -0500

    Start on styled-components for layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
 
 import Header from "./header";
 import "./layout.css";
