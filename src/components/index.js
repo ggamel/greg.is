10d71f53e55eaec060674dbdb260e958f75512bb
Greg Gamel <ggamel@gmail.com>
2019-05-02 21:54:48 -0500

    Rename index.mdx to index.js

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// import Card from "./Card";
+// import Footer from "./Footer";
+import Header from "./header";
+import Layout from "./layout";
+// import ProjectHeader from "./ProjectHeader";
+// import ProjectPagination from "./ProjectPagination";
+import SEO from "./seo";
+
+export { Header, Layout, SEO };
