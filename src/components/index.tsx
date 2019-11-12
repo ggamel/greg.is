94f14389ffae7ac8c9fc254ac3a51036783a053b
Greg Gamel <ggamel@gmail.com>
2019-11-11 20:58:05 -0600

    Remove Gatsby

--- src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-// import Card from "./Card";
-// import Footer from "./Footer";
-import Header from "./header";
-import Layout from "./layout";
-// import ProjectHeader from "./ProjectHeader";
-// import ProjectPagination from "./ProjectPagination";
-import SEO from "./seo";
-
-export { Header, Layout, SEO };
