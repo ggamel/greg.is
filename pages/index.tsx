f3b82fb59ffaff782e92228f4ae48f77ddb6ad32
Greg Gamel <ggamel@gmail.com>
2019-11-18 00:28:01 -0600

    Update main index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react";
 import Layout from "../components/Layout";
+import Link from "next/link";
 import { NextPage } from "next";
 
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Greg Gamel">
-      <h1>Greg Gamel</h1>
+      <h1 className="xl">Greg Gamel</h1>
+      <p>
+        <Link href="/styles">
+          <a>styles</a>
+        </Link>
+      </p>
     </Layout>
   );
 };
