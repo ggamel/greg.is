c59c2e5708f1c140d7c1ed5dc97065e38b76647a
Greg Gamel <ggamel@gmail.com>
2019-11-18 00:27:31 -0600

    Add styles page

---
+++ pages/styles.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import { NextPage } from "next";
+
+const StylesPage: NextPage = () => (
+  <Layout title="Styles | Greg Gamel">
+    <h1 className="xl">Styles Used on this Site</h1>
+    <h1>Greg Gamel</h1>
+    <h2>Greg Gamel</h2>
+    <h3>Greg Gamel</h3>
+    <h4>Greg Gamel</h4>
+    <p>These are the styles and components used on this website.</p>
+    <hr />
+  </Layout>
+);
+
+export default StylesPage;
