e2a7995c6bfee2e23ed78da9f7e400a3efae92c5
Greg Gamel <ggamel@gmail.com>
2020-01-16 09:16:54 -0600

    Config change for global .css in styled-jsx

--- next.config.js
+++ next.config.js
@@ -5,3 +5,22 @@ module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx"]
 });
 module.exports = withMDX();
+
+module.exports = {
+  webpack: (config, { defaultLoaders }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        defaultLoaders.babel,
+        {
+          loader: require("styled-jsx/webpack").loader,
+          options: {
+            type: "global"
+          }
+        }
+      ]
+    });
+
+    return config;
+  }
+};
