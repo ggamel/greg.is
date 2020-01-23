64e8099f6300fb45181c66cf3f8286185a2493bb
Greg Gamel <ggamel@gmail.com>
2020-01-22 23:32:18 -0600

    Remove colorMode toggle from index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, css, Styled, useColorMode } from "theme-ui";
+import { jsx } from "theme-ui";
 import Link from "next/link";
 
 import { SkipNavContent } from "@reach/skip-nav";
@@ -12,7 +12,6 @@ import { Global } from "@emotion/core";
 // import Nav from "../components/Nav";
 
 const IndexPage = ({ headerActive }) => {
-  const [colorMode, setColorMode] = useColorMode();
   return (
     <>
       <Page title="Home | Greg Gamel">
@@ -20,30 +19,6 @@ const IndexPage = ({ headerActive }) => {
         <Header />
         <article sx={{ minHeight: "100vh" }}>
           <main>
-            <button
-              sx={{
-                appearance: "none",
-                background: colorMode === "default" ? "#fff" : "red",
-                display: "inline-block",
-                textAlign: "center",
-                lineHeight: "inherit",
-                textDecoration: "none",
-                fontSize: "inherit",
-                fontWeight: "bold",
-                m: 0,
-                px: 3,
-                py: 2,
-                border: 0,
-                borderRadius: 4,
-                variant: "buttons.primary"
-              }}
-              onClick={e => {
-                setColorMode(colorMode === "default" ? "light" : "default");
-              }}
-            >
-              {" "}
-              {colorMode === "default" ? "🔆" : "🌙"}
-            </button>
             <h1 className="xl">Greg Gamel</h1>
             <p>
               Currently building <a href="https://eye.guide">EyeGuide</a>.
