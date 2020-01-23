2ad56ff89dafd59e25c9375f2218c39e56c6b4ab
Greg Gamel <ggamel@gmail.com>
2020-01-22 21:56:18 -0600

    Simplify _page.js due to theme-ui 0.3.0

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,15 @@
 import React from "react";
 import App from "next/app";
 import Router from "next/router";
-import { ThemeProvider, Styled, ColorMode } from "theme-ui";
+import { ThemeProvider, Styled } from "theme-ui";
 
 import theme from "../src/theme";
-
-// import { googlePageview } from "../components/GoogleAnalytics";
-
-// Router.events.on("routeChangeComplete", path => googlePageview(path));
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps, router } = this.props;
     return (
       <ThemeProvider theme={theme}>
-        <ColorMode />
         <Styled.root>
           <Component {...pageProps} query={router.query} />
         </Styled.root>
