2d99df45d99dcb8de97c127e9e7385a71a6325a8
Greg Gamel <ggamel@gmail.com>
2020-01-27 23:11:20 -0600

    Footer: fix colors, a for external, add setMode()

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,62 +6,76 @@ import Link from "./Link";
 const Footer = ({}) => {
   const [mode, setMode] = useColorMode();
   return (
-    <footer
+    <div
       sx={{
-        fontSize: 1,
-        variant: "styles.footer",
-        background: mode === "default" ? "#111" : "#111", // logic left for changes later
-        color: mode === "default" ? "#fff" : "#fff" // logic left for changes later
+        borderTop: mode === "dark" ? "1px solid #333" : "1px solid #eee",
+        background: mode === "dark" ? "#000" : "#fff" // logic left for changes later
       }}
     >
       <div
         sx={{
-          display: "grid",
-          gridTemplateRows: "repeat(4, 32px)",
-          gridTemplateColumns: ["repeat(2, 1fr)", "repeat(3, 1fr)"],
-          gridAutoFlow: "column",
-          px: 2,
-          py: 4
+          margin: "0 auto",
+          padding: "none",
+          maxWidth: "1000px"
         }}
       >
-        <Link href="/" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Home</Styled.a>
-        </Link>
-        <Link href="/about" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>About</Styled.a>
-        </Link>
-        <Link href="/writing" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Writing</Styled.a>
-        </Link>
-        <Link href="/styles" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Styles</Styled.a>
-        </Link>
-        <Link href="/work" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Work</Styled.a>
-        </Link>
-        <Link href="/community" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Projects</Styled.a>
-        </Link>
-        <Link href="/support" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>APOD</Styled.a>
-        </Link>
-        <Link href="/contact" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Grad</Styled.a>
-        </Link>
-        <Link href="/support" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Daily.is</Styled.a>
-        </Link>
-        <Link href="/products" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Experiments</Styled.a>
-        </Link>
-        <Link href="/contact" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Contact</Styled.a>
-        </Link>
-        <Link href="/links" sx={{ variant: "styles.navlink", p: 2 }}>
-          <Styled.a>Links</Styled.a>
-        </Link>
-      </div>
+        <footer
+          sx={{
+            fontSize: 1,
+            variant: "styles.footer",
 
+            color: mode === "dark" ? "#fff" : "#000" // logic left for changes later
+          }}
+        >
+          <div
+            sx={{
+              display: "grid",
+              gridTemplateRows: "repeat(4, 32px)",
+              gridTemplateColumns: ["repeat(2, 1fr)", "repeat(3, 1fr)"],
+              gridAutoFlow: "column",
+              px: 4,
+              py: 4
+            }}
+          >
+            <Link href="/" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Home</Styled.a>
+            </Link>
+            <Link href="/about" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>About</Styled.a>
+            </Link>
+            <Link href="/writing" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Writing</Styled.a>
+            </Link>
+            <Link href="/styles" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Styles</Styled.a>
+            </Link>
+            <Link href="/work" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Work</Styled.a>
+            </Link>
+            <Link href="/community" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Projects</Styled.a>
+            </Link>
+            <Link href="/support" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>APOD</Styled.a>
+            </Link>
+            <Link href="/contact" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Grad</Styled.a>
+            </Link>
+            <Link href="/support" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Daily.is</Styled.a>
+            </Link>
+            <Link href="/products" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Experiments</Styled.a>
+            </Link>
+            <Link href="/contact" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Contact</Styled.a>
+            </Link>
+            <Link href="/links" sx={{ variant: "styles.navlink", p: 2 }}>
+              <Styled.a>Links</Styled.a>
+            </Link>
+          </div>
+        </footer>
+      </div>
       <div
         sx={{
           display: "flex",
@@ -76,22 +90,22 @@ const Footer = ({}) => {
             pr: 3
           }}
           onClick={e => {
-            setMode(mode === "default" ? "light" : "default");
+            const next = mode === "dark" ? "light" : "dark";
+            setMode(next);
           }}
         >
-          {" "}
-          {mode === "default" ? "🔆" : "🌙"}
+          {mode === "dark" ? "🔆" : "🌙"}
         </a>
-        <Link href="https://github.com/ggamel" sx={{ color: "inherit" }}>
+        <a href="https://github.com/ggamel" sx={{ color: "inherit" }}>
           <Styled.a>GitHub</Styled.a>
-        </Link>
+        </a>
         <div sx={{ mx: 1 }} />
-        <Link href="https://twitter.com/ggamel" sx={{ color: "inherit" }}>
+        <a href="https://twitter.com/ggamel" sx={{ color: "inherit" }}>
           <Styled.a>Twitter</Styled.a>
-        </Link>
+        </a>
         <div sx={{ mx: 1 }} />© 2020 Greg Gamel
       </div>
-    </footer>
+    </div>
   );
 };
 export default Footer;
