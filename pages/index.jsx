48dd54c3ec6717f5cb7911ecc267dfbfa5a6668a
Greg Gamel <ggamel@gmail.com>
2020-01-27 23:13:08 -0600

    Start using Box primitive for layout. Fix colors

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui";
+import { jsx, useColorMode, Box } from "theme-ui";
 import Link from "next/link";
 
 import { SkipNavContent } from "@reach/skip-nav";
@@ -12,32 +12,43 @@ import { Global } from "@emotion/core";
 // import Nav from "../components/Nav";
 
 const IndexPage = ({ headerActive }) => {
+  const [mode, setMode] = useColorMode();
   return (
     <>
       <Page title="Home | Greg Gamel">
         <SkipNavContent tabIndex="0" />
         <Header />
-        <article sx={{ minHeight: "100vh" }}>
-          <main>
-            <h1 className="xl">Greg Gamel</h1>
-            <p>
-              Currently building <a href="https://eye.guide">EyeGuide</a>.
-              Designer & developer of hardware & software products.
-            </p>
-            <p>Team Player. Teaches. No-Fuss. 💚s Green Chile.</p>
-            <ul>
-              <li>
-                <a href="https://twitter.com/ggamel">Twitter</a>
-              </li>
-              <li>
-                <a href="https://github.com/ggamel">GitHub</a>
-              </li>
-              <li>
-                <a href="mailto:g@greg.is">✍️ Email</a>
-              </li>
-            </ul>
-          </main>
-        </article>
+
+        <Box sx={{ background: mode === "dark" ? "#111" : "#fafafa" }}>
+          <article
+            sx={{
+              minHeight: "100vh",
+              width: "100%",
+              maxWidth: "1024px",
+              margin: "0 auto"
+            }}
+          >
+            <main>
+              <h1 className="xl">Greg Gamel</h1>
+              <p>
+                Currently building <a href="https://eye.guide">EyeGuide</a>.
+                Designer & developer of hardware & software products.
+              </p>
+              <p>Team Player. Teaches. No-Fuss. 💚s Green Chile.</p>
+              <ul>
+                <li>
+                  <a href="https://twitter.com/ggamel">Twitter</a>
+                </li>
+                <li>
+                  <a href="https://github.com/ggamel">GitHub</a>
+                </li>
+                <li>
+                  <a href="mailto:g@greg.is">✍️ Email</a>
+                </li>
+              </ul>
+            </main>
+          </article>
+        </Box>
       </Page>
 
       <Footer />
