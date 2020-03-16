449d8654484181d5c62492ed7e98bea96519bb0f
Greg Gamel <ggamel@gmail.com>
2020-03-15 21:31:21 -0500

    Remove webpack config for styled-jsx

--- next.config.js
+++ next.config.js
@@ -1,26 +1,7 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
 });
+
 module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "md", "mdx"]
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 });
-module.exports = withMDX();
-
-module.exports = {
-  webpack: (config, { defaultLoaders }) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        defaultLoaders.babel,
-        {
-          loader: require("styled-jsx/webpack").loader,
-          options: {
-            type: "global"
-          }
-        }
-      ]
-    });
-
-    return config;
-  }
-};
